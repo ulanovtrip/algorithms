88a6cfe4e67625269086e7a21ed0a74288013951
iulanov <iulanov@swiftcom.uk>
2020-09-18 11:42:09 +0300

    18/09/2020 11:41 AM
    Tasks: 39, 40, 41, 42

--- src/petrs/analysis/Task_39.java
+++ src/petrs/analysis/Task_39.java
@@ -1,8 +1,37 @@
 package petrs.analysis;
 
+import petrs.RandomArray;
+import petrs.Utils;
+
 /**
- *Дан массив. Найдите три последовательных элемента в массиве, сумма которых максимальна.
+ * Дан массив. Найдите три последовательных элемента в массиве, сумма которых максимальна.
  */
 
 public class Task_39 {
+    public static void main(String[] args) {
+        RandomArray inputArray = new RandomArray(10, 13);
+        inputArray.generate(1);
+        inputArray.printIntArray();
+        int index1 = 0;
+        int index2 = 1;
+        int index3 = 2;
+        int maxSumOfElements = inputArray.getElement(index1)
+                + inputArray.getElement(index2)
+                + inputArray.getElement(index3);
+
+        for (int i = 1; i < inputArray.getSize() - 2; i++) {
+            int j = i + 1;
+            int k = i + 2;
+            int newMaxSum = inputArray.getElement(i) + inputArray.getElement(j) + inputArray.getElement(k);
+            if (newMaxSum > maxSumOfElements) {
+                index1 = i;
+                index2 = j;
+                index3 = k;
+                maxSumOfElements = newMaxSum;
+            }
+        }
+        Utils.newLine();
+        Utils.print("index1= " + index1 + " / " + "index2= " + index2 + " / " + "index3= " + index3);
+        Utils.print("maxSumOfElements = " + maxSumOfElements);
+    }
 }
--- src/petrs/analysis/Task_40.java
+++ src/petrs/analysis/Task_40.java
@@ -1,8 +1,28 @@
 package petrs.analysis;
 
+import petrs.RandomArray;
+import petrs.Utils;
+
 /**
- *В данном массиве найдите количество чисел, соседи у которых отличаются более чем в 2 раза.
+ * В данном массиве найдите количество чисел, соседи у которых отличаются более чем в 2 раза.
  */
 
 public class Task_40 {
+    public static void main(String[] args) {
+        RandomArray inputArray = new RandomArray(30, 23);
+        inputArray.generate(1);
+        inputArray.printIntArray();
+        int countOfNumbers = 0;
+
+        for (int i = 1; i < inputArray.getSize() - 1; i++) {
+            int leftNeighbour = Math.abs(inputArray.getElement(i - 1));
+            int rightNeighbour = Math.abs(inputArray.getElement(i + 1));
+            int squareOfCurrent = (int) Math.pow(inputArray.getElement(i), 2);
+            if (leftNeighbour > squareOfCurrent && rightNeighbour > squareOfCurrent) {
+                countOfNumbers++;
+            }
+        }
+        Utils.newLine();
+        Utils.print("countOfNumbers = " + countOfNumbers);
+    }
 }
