cd8dcf26b8de2510e50e94e44963e1f390014173
iulanov <iulanov@swiftcom.uk>
2020-09-21 14:32:01 +0300

    21/09/2020 2:31 PM
    Правки: 39, 40, 41.
    Решено: 42

--- src/petrs/analysis/Task_42.java
+++ src/petrs/analysis/Task_42.java
@@ -3,51 +3,44 @@ package petrs.analysis;
 import petrs.RandomArray;
 import petrs.Utils;
 
+import static petrs.Utils.newLine;
+
 /**
- * Проверьте, содержит ли данный массив из n чисел, все числа от 1 до n.
+ * Проверьте, содержит ли данный массив из N чисел, все числа от 1 до N.
  */
 
 public class Task_42 {
     public static void main(String[] args) {
-        RandomArray inputArray = new RandomArray(10, 9);
-        inputArray.generate(1);
+        int N = 10;
+        RandomArray inputArray = new RandomArray(N, 10);
+        //генерится рандом
+        inputArray.generate(0);
+
+        //генерится для проверки арифметическая от 0 до N
+        //inputArray.generateSorted();
+
+        //соритирую рандом
+        inputArray.sort();
         inputArray.printIntArray();
-        int N = inputArray.getSize() - 1;
-        int[] bandZeroToN = new int[N];
-        int countFindNumbers = 0;
+        newLine();
 
-        for (int i = 0; i < N; i++) {
-            bandZeroToN[i] = i + 1;
-        }
-
-        for (int i = 0; i < inputArray.getSize(); i++) {
-            int number = inputArray.getElement(i);
-            if (binarySearch(number, bandZeroToN)) {
-                countFindNumbers++;
-            }
-        }
-
-        if (countFindNumbers + 2 == N) {
-            Utils.print("Success");
-        }
+        isContainsFromZeroToN(inputArray);
     }
 
-    static boolean binarySearch(int number, int[] band) {
-        int loPosition = 0;
-        int hiPosition = band.length;
-        while (loPosition <= hiPosition) {
-            int middlePosition = (hiPosition + loPosition) / 2;
-            if (number < band[middlePosition]) {
-                hiPosition = middlePosition - 1;
-            } else if (number > band[middlePosition]) {
-                loPosition = middlePosition + 1;
-            } else {
-                return true;
+    private static boolean isContainsFromZeroToN(RandomArray inputArray) {
+        for (int i = 0; i < inputArray.getSize(); i++) {
+            if (inputArray.isEmpty()) {
+                System.out.println("input array is empty!");
+                break;
             }
-            if (loPosition == hiPosition) {
+            if (i == inputArray.getElement(i)) {
+                continue;
+            } else {
+                System.out.println("Fail");
                 return false;
             }
         }
+        System.out.println("Success");
         return true;
     }
 }
--- src/petrs/analysis/Task_43.java
+++ src/petrs/analysis/Task_43.java
@@ -1,11 +1,45 @@
 package petrs.analysis;
 
+import petrs.RandomArray;
+
 /**
  * Проверьте, образуют ли элементы массива в данном порядке арифметическую или геометрическую прогрессии.
  */
 
 public class Task_43 {
     public static void main(String[] args) {
+        RandomArray inputArithmetic = new RandomArray(5, 10);
+        RandomArray inputGeometric = new RandomArray(6, 10);
 
+        inputArithmetic.generateArithmeticProgression(1);
+        inputGeometric.generateGeometricProgression(2);
+
+        System.out.println(isArithmetic(inputArithmetic, 1));
+        System.out.println(isGeometric(inputGeometric));
+    }
+
+
+    private static boolean isArithmetic(RandomArray array, int stepProgression) {
+        if (array.getSize() < 2) return false;
+
+        for (int i = 0; i < array.getSize(); i++) {
+            if ((array.getElement(i) + stepProgression) - array.getElement(i)
+                    != array.getElement(1) - array.getElement(0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isGeometric(RandomArray array) {
+        if (array.getSize() < 2) return false;
+        double eps = 1e-3;
+        for (int i = 1; i < array.getSize() - 1; i++) {
+            if (Math.abs(array.getElement(i) * array.getElement(i) -
+                    array.getElement(i - 1) * array.getElement(i + 1)) > eps) {
+                return false;
+            }
+        }
+        return true;
     }
 }
