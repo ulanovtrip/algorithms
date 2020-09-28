e03ccfdadc1983d99af62f61add43d044406d013
iulanov <iulanov@swiftcom.uk>
2020-09-28 11:48:58 +0300

    28/09/2020 11:48 PM
    Правки:
    Решено: 50, 51, 52

--- src/petrs/analysis/Task_51.java
+++ src/petrs/analysis/Task_51.java
@@ -1,8 +1,34 @@
 package petrs.analysis;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
- *В данном массиве найдите два наименьших элемента.
+ * В данном массиве найдите два наименьших элемента.
  */
 
 public class Task_51 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(" ");
+        int[] numbers = new int[input.length];
+
+        try {
+            for (int i = 0; i < input.length; i++) {
+                numbers[i] = Integer.parseInt(input[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("Input is empty!!!");
+        }
+
+        findTwoSmallestElements(numbers);
+    }
+
+    private static void findTwoSmallestElements(int[] numbers) {
+        Arrays.sort(numbers);
+        int firstMinimum = numbers[0];
+        int secondMinimum = numbers[1];
+        System.out.println("firstMinimum = " + firstMinimum);
+        System.out.println("secondMinimum = " + secondMinimum);
+    }
 }
--- src/petrs/analysis/Task_52.java
+++ src/petrs/analysis/Task_52.java
@@ -1,8 +1,45 @@
 package petrs.analysis;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
- *Определите, есть ли в массиве повторяющиеся элементы.
+ * Определите, есть ли в массиве повторяющиеся элементы.
  */
 
 public class Task_52 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(" ");
+        int[] numbers = new int[input.length];
+
+        try {
+            for (int i = 0; i < input.length; i++) {
+                numbers[i] = Integer.parseInt(input[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("Input is empty!!!");
+        }
+
+        findRepeatingElements(numbers);
+    }
+
+    private static void findRepeatingElements(int[] numbers) {
+        Arrays.sort(numbers);
+        int countOfRepeating = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j]) {
+                    countOfRepeating++;
+
+                }
+            }
+        }
+
+        if (countOfRepeating == 0) {
+            System.out.println("All elements is unique!!!");
+        } else {
+            System.out.println("There are the same elements!!!");
+        }
+    }
 }
