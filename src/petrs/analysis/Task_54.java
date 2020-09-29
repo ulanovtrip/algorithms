f0a50272029656f64d7a6726e567fadd923cdea6
iulanov <iulanov@swiftcom.uk>
2020-09-29 11:52:30 +0300

    29/09/2020 11:52 AM
    Правки:
    Решено: 53, 54, 55
    54 нужно проговорить. Не понял. Загуглил.

--- src/petrs/analysis/Task_54.java
+++ src/petrs/analysis/Task_54.java
@@ -1,8 +1,33 @@
 package petrs.analysis;
 
 /**
- *В массиве найдите количество серий из четверок подряд идущих попарно различных элементов.
+ * В массиве найдите количество серий из четверок подряд идущих попарно различных элементов.
  */
 
 public class Task_54 {
+    public static void main(String[] args) {
+        int[] pairsOfFours = {2, 3, 4, 5, 5, 6, 7, 8, 9, 9, 9};
+        findCountOfFours(pairsOfFours);
+    }
+
+    private static void findCountOfFours(int[] pairsOfFours) {
+        int countFours = 0;
+        for (int i = 0; i < pairsOfFours.length - 4; i++) {
+            if (isSeriesOfFour(pairsOfFours, i)) {
+                countFours += 1;
+            }
+        }
+        System.out.println("серий из четверок подряд идущих попарно различных элементов = " + countFours);
+    }
+
+    private static boolean isSeriesOfFour(int[] pairOfFour, int startIndex) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = i + 1; j < 4; j++) {
+                if (pairOfFour[startIndex + i] == pairOfFour[startIndex + j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- src/petrs/analysis/Task_55.java
+++ src/petrs/analysis/Task_55.java
@@ -1,11 +1,30 @@
 package petrs.analysis;
 
 /**
- * Определите, можно ли вычеркнуть из данного массива одно число так, чтобы оставшиеся числа оказались упорядоченными по возрастанию.
+ * Определите, можно ли вычеркнуть из данного массива одно число так,
+ * чтобы оставшиеся числа оказались упорядоченными по возрастанию.
  */
 
 public class Task_55 {
     public static void main(String[] args) {
+        int[] inputArray = {1, 2, 3, 4, 5, 3, 6, 7, 8, 9};
+        check(inputArray);
+    }
 
+    private static void check(int[] inputArray) {
+        boolean result = true;
+        boolean flag = false;
+
+        for (int i = 0; i < inputArray.length - 2; i++) {
+            if (inputArray[i] > inputArray[i + 1]) {
+                if (flag || inputArray[i] > inputArray[i + 2]) {
+                    result = false;
+                    break;
+                } else {
+                    flag = true;
+                }
+            }
+        }
+        System.out.println("Result = " + result);
     }
 }
