149dd284f3c64ea657c09abd46a462fedbe8aad0
iulanov <iulanov@swiftcom.uk>
2020-11-29 18:35:17 +0300

    2020/11/29 6:35 PM Правки: Решено: 1,3,4,5 in chapter 14

---
+++ src/tij4/chapter_14/exercise_3/Shapes.java
@@ -0,0 +1,45 @@
+package tij4.chapter_14.exercise_3;
+
+import java.util.*;
+
+abstract class Shape {
+    void draw() {
+        System.out.println(this + ".draw()");
+    }
+
+    abstract public String toString();
+}
+
+class Circle extends Shape {
+    public String toString() {
+        return "Circle";
+    }
+}
+
+class Square extends Shape {
+    public String toString() {
+        return "Square";
+    }
+}
+
+class Triangle extends Shape {
+    public String toString() {
+        return "Triangle";
+    }
+}
+
+class Rhomboid extends Shape {
+    public String toString() {
+        return "Rhomboid";
+    }
+}
+
+public class Shapes {
+    public static void main(String[] args) {
+        Rhomboid rhomboid = new Rhomboid();
+        ((Shape) rhomboid).draw();
+
+        //inconvertible types
+        //((Circle) rhomboid).draw();
+    }
+}
