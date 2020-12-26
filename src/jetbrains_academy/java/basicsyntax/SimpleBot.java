fae55dfadd444f227f6afbd48c45e3a94d1766c0
iulanov <iulanov@swiftcom.uk>
2020-12-26 14:55:56 +0300

    Правки: Решено: jet_26

--- src/jetbrains_academy/java/basicsyntax/SimpleBot.java
+++ src/jetbrains_academy/java/basicsyntax/SimpleBot.java
@@ -1,11 +1,14 @@
 package jetbrains_academy.java.basicsyntax;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SimpleBot {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+    final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
@@ -22,5 +25,51 @@ public class SimpleBot {
         int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
 
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        int userNumber = scanner.nextInt();
+
+        for (int i = 0; i <= userNumber; i++) {
+            System.out.println(i + "!");
+        }
+
+        test();
+        end();
+    }
+
+    static void test() {
+        List<String> answers = new LinkedList<>();
+        answers.add("1. To repeat a statement multiple times.");
+        answers.add("2. To decompose a program into several small subroutines.");
+        answers.add("3. To determine the execution time of a program.");
+        answers.add("4. To interrupt the execution of a program.");
+
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods?");
+
+        for (String s : answers) {
+            System.out.println(s);
+        }
+
+        String wrongAns = "Please, try again.";
+
+        boolean flag = false;
+        while (!flag) {
+            int userAnswer = scanner.nextInt();
+            switch (userAnswer) {
+                case (1):
+                case (3):
+                case (4):
+                    System.out.println(wrongAns);
+                    continue;
+                case (2):
+                    flag = true;
+                    break;
+            }
+        }
+    }
+
+    static void end() {
+        System.out.println("Congratulations, have a nice day!"); // Do not change this text
     }
 }
---
+++ src/stepik/methods_1/greedy_algorithms/PointCover.java
@@ -0,0 +1,71 @@
+package stepik.methods_1.greedy_algorithms;
+
+// https://razilov-code.ru/2018/10/16/cover-intervals-with-points/
+
+import java.util.*;
+
+class Spot implements Comparable {
+    int numOtrezok;
+    int spotval;
+    char side;
+
+    public Spot(int numOtrezok, int spotval, char side) {
+        this.numOtrezok = numOtrezok;
+        this.spotval = spotval;
+        this.side = side;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Spot spot = (Spot) o;
+        int numbers = this.spotval > spot.spotval ? 1 : this.spotval < spot.spotval ? -1 : 0;
+        switch (numbers) {
+            case 0:
+                return side == 'R' ? 1 : -1;
+            default:
+                return numbers;
+        }
+    }
+}
+
+public class PointCover {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Количество отрезков
+        int n = sc.nextInt();
+        int[][] otr = new int[n][];
+        sc.nextLine();
+        List<Spot> listSpots = new ArrayList<>();
+        List<Integer> stack = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+
+        //Получаем края отрезков и записываем в массив
+        for (int i = 0; i < n; i++) {
+            String[] split = sc.nextLine().split(" ");
+            otr[i] = new int[2];
+            otr[i][0] = Integer.parseInt(split[0]);
+            otr[i][1] = Integer.parseInt(split[1]);
+        }
+
+        //Заполняем массив точек
+        for (int i = 0; i < n; i += 1) {
+            listSpots.add(new Spot(i + 1, otr[i][0], 'L'));
+            listSpots.add(new Spot(i + 1, otr[i][1], 'R'));
+        }
+
+        //Сортируем массив точек
+        listSpots.sort(Comparator.naturalOrder());
+
+        //Следуем алгоритму, описанному в статье
+        for (int i = 0; i < listSpots.size(); i++) {
+            if (listSpots.get(i).side == 'L')
+                stack.add(listSpots.get(i).numOtrezok);
+            else if (stack.contains(listSpots.get(i).numOtrezok)) {
+                result.add(listSpots.get(i).spotval);
+                stack.clear();
+            }
+        }
+        System.out.println(result.size());
+        result.forEach(integer -> System.out.print(integer + " "));
+    }
+}
