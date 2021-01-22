9fc7403328036c9d3c98ab14ffaf82fd2c47b532
uis <veloteh@outlook.com>
2021-01-22 23:46:32 +0300

    22_january

---
+++ src/jetbrains_academy/java/Test.java
@@ -0,0 +1,50 @@
+package jetbrains_academy.java;
+
+public class Test {
+
+    public static void main(String args[]) {
+        BaseNumberGenerator generator = new MagicNumberGenerator(10);
+        System.out.print(generator.generate());
+    }
+}
+
+class BaseNumberGenerator {
+
+    protected int base;
+
+    public BaseNumberGenerator(int base) {
+        this.base = base;
+    }
+
+    public int generate() {
+        return base + 11;
+    }
+}
+
+class NumberGenerator extends BaseNumberGenerator {
+
+    public NumberGenerator(int base) {
+        super(base);
+    }
+
+    @Override
+    public int generate() {
+        return super.generate() + getNumber();
+    }
+
+    protected int getNumber() {
+        return this.base - 7;
+    }
+}
+
+class MagicNumberGenerator extends NumberGenerator {
+
+    public MagicNumberGenerator(int base) {
+        super(base);
+    }
+
+    @Override
+    protected int getNumber() {
+        return this.base + 7;
+    }
+}
