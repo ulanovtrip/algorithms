3fc834e54f71645ea283fef788cc79438cadb8b6
iulanov <iulanov@swiftcom.uk>
2021-01-26 21:03:02 +0300

    26_january

---
+++ src/jetbrains_academy/java/patterns/strategy/PickYourTeamMain.java
@@ -0,0 +1,115 @@
+package jetbrains_academy.java.patterns.strategy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// https://hyperskill.org/learn/step/3329
+class SelectionContext {
+
+    private PersonSelectionAlgorithm algorithm;
+
+    public void setAlgorithm(PersonSelectionAlgorithm algorithm) {
+        // write your code here
+        this.algorithm = algorithm;
+    }
+
+    public Person[] selectPersons(Person[] persons) {
+        // write your code here
+        return algorithm.select(persons);
+    }
+}
+
+interface PersonSelectionAlgorithm {
+
+    Person[] select(Person[] persons);
+}
+
+class TakePersonsWithStepAlgorithm implements PersonSelectionAlgorithm {
+
+    int step;
+
+    public TakePersonsWithStepAlgorithm(int step) {
+        // write your code here
+        this.step = step;
+    }
+
+    @Override
+    public Person[] select(Person[] persons) {
+        // write your code here
+        if (step == 1) {
+            return persons;
+        }
+        Person[] person = new Person[(int) Math.ceil((double) persons.length / (double) step)];
+        int i = 0;
+        for (int j = 0; j < person.length; j++) {
+            person[j] = persons[i];
+            i += step;
+        }
+        return person;
+    }
+}
+
+class TakeLastPersonsAlgorithm implements PersonSelectionAlgorithm {
+
+    int count;
+
+    public TakeLastPersonsAlgorithm(int count) {
+        // write your code here
+        this.count = count;
+    }
+
+    @Override
+    public Person[] select(Person[] persons) {
+        // write your code here
+        return Arrays.copyOfRange(persons, persons.length - count, persons.length);
+    }
+}
+
+class Person {
+
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
+
+/* Do not change code below */
+public class PickYourTeamMain {
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        final int count = Integer.parseInt(scanner.nextLine());
+        final Person[] persons = new Person[count];
+
+        for (int i = 0; i < count; i++) {
+            persons[i] = new Person(scanner.nextLine());
+        }
+
+        final String[] configs = scanner.nextLine().split("\\s+");
+
+        final PersonSelectionAlgorithm alg = create(configs[0], Integer.parseInt(configs[1]));
+        SelectionContext ctx = new SelectionContext();
+        ctx.setAlgorithm(alg);
+
+        final Person[] selected = ctx.selectPersons(persons);
+        for (Person p : selected) {
+            System.out.println(p.name);
+        }
+    }
+
+    public static PersonSelectionAlgorithm create(String algType, int param) {
+        switch (algType) {
+            case "STEP": {
+                return new TakePersonsWithStepAlgorithm(param);
+            }
+            case "LAST": {
+                return new TakeLastPersonsAlgorithm(param);
+            }
+            default: {
+                throw new IllegalArgumentException("Unknown algorithm type " + algType);
+            }
+        }
+    }
+}
