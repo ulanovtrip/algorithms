07a194dbd62fdb0c127b72ed0f11aaab7742e9fc
iulanov <iulanov@swiftcom.uk>
2021-02-05 17:13:17 +0300

    5_feb

---
+++ src/itis/topic_3/TheEquation.java
@@ -0,0 +1,13 @@
+package itis.topic_3;
+
+import java.util.Scanner;
+
+public class TheEquation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int memberA = scanner.nextInt();
+        int memberB = scanner.nextInt();
+
+        int solution =
+    }
+}
